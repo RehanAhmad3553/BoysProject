d44ffe40f779ec7a673a614daacefb4a844ef3d4
RehanAhmad3553 <117620259+RehanAhmad3553@users.noreply.github.com>
2023-09-05 20:52:12 +0500

    First Time

---
+++ app/src/main/java/com/quizapplications/testproject/MainActivity.java
@@ -0,0 +1,42 @@
+package com.quizapplications.testproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.quizapplications.testproject.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
